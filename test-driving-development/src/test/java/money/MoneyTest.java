01cc413eaf58ef25d846faab4a496cf9f78d390b
yukiko <springcomesagain129@gmail.com>
2018-12-25 08:25:35 +0000

    make first test

---
+++ test-driving-development/src/test/java/money/MoneyTest.java
@@ -0,0 +1,16 @@
+package money;
+
+
+
+public class MoneyTest {
+    
+    
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+    
+    p
+}
