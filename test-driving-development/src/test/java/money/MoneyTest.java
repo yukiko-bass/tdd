28d6dcf02e1d400c84e72662bb81027e8c76f94b
yukiko kato <kato_y@auroraworks.co.jp>
2018-12-25 17:46:49 +0900

    make first test

--- test-driving-development/src/test/java/money/MoneyTest.java
+++ test-driving-development/src/test/java/money/MoneyTest.java
@@ -1,16 +1,17 @@
 package money;
 
 
+import org.junit.Test;
 
 public class MoneyTest {
-    
-    
+
+
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
     }
-    
-    p
+
+
 }
