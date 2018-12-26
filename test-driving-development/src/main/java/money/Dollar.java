0d963d8885f530dfda7f65f48a83866f5461f630
yukiko kato <kato_y@auroraworks.co.jp>
2018-12-26 17:18:56 +0900

    #13 timesの一般化

--- test-driving-development/src/main/java/money/Dollar.java
+++ test-driving-development/src/main/java/money/Dollar.java
@@ -4,7 +4,4 @@ public class Dollar extends Money{
     Dollar(int amount, String currency) {
         super(amount, currency);
     }
-    Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
-    }
 }
--- test-driving-development/src/main/java/money/Money.java
+++ test-driving-development/src/main/java/money/Money.java
@@ -1,13 +1,15 @@
 package money;
 
-public abstract class Money {
+public class Money {
     protected int amount;
     protected String currency;
     Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
     String currency() {
         return currency;
     }
@@ -15,12 +17,17 @@ public abstract class Money {
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
     }
     static Money dollar(int amount) {
         return new Dollar(amount, "USD");
     }
     static Money franc(int amount) {
         return new Franc(amount, "CHF");
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
