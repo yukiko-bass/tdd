8056319e66551bb0e6a124339027797689633fff
yukiko kato <kato_y@auroraworks.co.jp>
2019-01-02 22:02:19 +0900

    #25 setUpを最初に呼び出す

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -2,26 +2,36 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def set_up(self):
+        pass
+
     def run(self):
+        self.set_up()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
+    def set_up(self):
+        self.was_run = None
+        self.was_set_up = 1
 
-    def testMethod(self):
-        self.wasRun = 1
+    def test_method(self):
+        self.was_run = 1
 
 
 class TestCaseTest(TestCase):
-    def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+    def set_up(self):
+        self.test = WasRun("test_method")
+
+    def test_running(self):
+        self.test.run()
+        assert(self.test.was_run)
+
+    def test_set_up(self):
+        self.test.run()
+        assert(self.test.was_set_up)
 
 
-TestCaseTest("testRunning").run()
+TestCaseTest("test_running").run()
+TestCaseTest("test_set_up").run()
