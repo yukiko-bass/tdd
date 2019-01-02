41a320cdba781c931172bf0481724e473da59145
yukiko kato <kato_y@auroraworks.co.jp>
2019-01-02 13:58:35 +0900

    no message

--- test-driving-development/src/main/java/money/Money.java
+++ test-driving-development/src/main/java/money/Money.java
@@ -14,7 +14,6 @@ public class Money implements Expression{
         return new Sum(this, addend);
     }
     public Money reduce(Bank bank, String to) {
-//        return this;
         int rate = bank.rate(currency, to);
         return new Money(amount / rate, to);
     }
