7a34e9782a35cf79614203c194c77b51999f3f43
yukiko kato <kato_y@auroraworks.co.jp>
2019-01-02 11:57:18 +0900

    #1 $5 + 10 CHF = $10(レートが2:1の場合)

--- test-driving-development/src/main/java/money/Money.java
+++ test-driving-development/src/main/java/money/Money.java
@@ -7,10 +7,10 @@ public class Money implements Expression{
         this.amount = amount;
         this.currency = currency;
     }
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
-    Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
     public Money reduce(Bank bank, String to) {
