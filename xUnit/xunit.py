50026280429999ee48d0c43d62ff14703643b153
yukiko kato <kato_y@auroraworks.co.jp>
2019-01-04 12:52:17 +0900

    #29 収集したテスト結果を出力する

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -6,13 +6,27 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.test_started()
         self.set_up()
         method = getattr(self, self.name)
         method()
         self.tear_down()
+        return result
 
     def tear_down(self):
         pass
+
+
+class TestResult:
+    def __init__(self):
+        self.run_count = 0
+
+    def test_started(self):
+        self.run_count = self.run_count + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.run_count
 
 
 class WasRun(TestCase):
@@ -22,14 +36,30 @@ class WasRun(TestCase):
     def test_method(self):
         self.log = self.log + "test_method "
 
+    def test_broken_method(self):
+        raise Exception
+
     def tear_down(self):
         self.log = self.log + "tear_down "
 
+
 class TestCaseTest(TestCase):
     def test_template_method(self):
-        self.test = WasRun("test_method")
-        self.test.run()
-        assert("set_up test_method tear_down " == self.test.log)
+        test = WasRun("test_method")
+        test.run()
+        assert("set_up test_method tear_down " == test.log)
+
+    def test_result(self):
+        test = WasRun("test_method")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def test_failed_result(self):
+        test = WasRun("test_broken_method")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 
 TestCaseTest("test_template_method").run()
+TestCaseTest("test_result").run()
+#TestCaseTest("test_failed_result").run()
